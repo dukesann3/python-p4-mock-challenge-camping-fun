2451417a0d97b1e2f1048643ab087ed55bf31328
Duke <dukemorise33@gmail.com>
2024-02-07 15:47:08 -0800

    completed lesson

--- server/app.py
+++ server/app.py
@@ -20,10 +20,115 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 
 @app.route('/')
 def home():
     return ''
+
+class Campers(Resource):
+
+    def get(self):
+        campers = [camper.to_dict(rules=('-signups',)) for camper in Camper.query.all()]
+        return make_response(campers, 200)
+    
+    def post(self):
+        #post request here
+        data = request.get_json()
+
+        try:
+            camper = Camper(
+                name=data["name"],
+                age=data["age"]
+            )
+            db.session.add(camper)
+            db.session.commit()
+            return make_response(camper.to_dict(rules=('-signups',)), 201)
+        except:
+            return make_response(
+                { "errors": ["validation errors"] }, 400
+            )
+        
+class CamperID(Resource):
+
+    def get(self, id):
+        camper = Camper.query.filter(Camper.id == id).first()
+        if camper:
+            return make_response(camper.to_dict(), 200)
+        else:
+            return make_response(
+                {'error': "Camper not found"}, 404
+            )
+    
+    def patch(self, id):
+        camper = Camper.query.filter_by(id=id).first()
+        if not camper:
+            return make_response({"error": "Camper not found"}, 404)
+        
+        try:
+            data = request.get_json()
+
+            for attr in data:
+                print(attr)
+                setattr(camper, attr, data[attr])
+
+            db.session.add(camper)
+            db.session.commit()
+
+            return make_response(camper.to_dict(rules=("-signups",)), 202)
+        
+        except:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+class Activities(Resource):
+    def get(self):
+        activities = [activity.to_dict(rules=("-signups",)) for activity in Activity.query.all()]
+        return make_response(activities, 200)
+
+class ActivitiesID(Resource):
+    def delete(self, id):
+        activity = Activity.query.filter_by(id=id).first()
+
+        if activity:
+            db.session.delete(activity)
+            db.session.commit()
+
+            return make_response({},204)
+        else:
+            return make_response(
+                {"error": "Activity not found"}, 404
+            )
+        
+class Signups(Resource):
+    def post(self):
+        data = request.get_json()
+
+        try:
+            signup = Signup(
+                time=data["time"],
+                activity_id=data["activity_id"],
+                camper_id=data["camper_id"]
+            )
+
+            db.session.add(signup)
+            db.session.commit()
+            return make_response(signup.to_dict(), 201)
+        
+        except:
+            return make_response({ "errors": ["validation errors"] }, 400)
+
+        
+
+
+
+
+api.add_resource(CamperID, '/campers/<int:id>')
+api.add_resource(Campers, '/campers')
+api.add_resource(Activities, '/activities')
+api.add_resource(ActivitiesID, '/activities/<int:id>')
+api.add_resource(Signups, '/signups')
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,12 +21,32 @@ class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     difficulty = db.Column(db.Integer)
 
-    # Add relationship
+    #By creating a db.relionship, it allows the dev to not create a physical foreign key to 
+    #relate one model to another. So, when instantiating a new class, instead of inputting the 
+    #foreign key, the user will simply add the model object straight in.
+
+    #back_populates vs backref...
+    #back ref automatically relates the relating model, so you wouldn't have to reciprocally 
+    #create a db.relationip... for the other side
+    #However, even to me, I will be confused and hard to keep track of what is actually being 
+    #related. So, it is generally recommended to use back_populates!
+
+    signups = db.relationship('Signup', back_populates='activity', cascade='all, delete-orphan')
+    #back_populates is relating to the current table
+    #campers = db.relationship('Camper', secondary="signups", back_populates='activities')
+    #use association proxies instead!
+
+    campers = association_proxy('signups', 'camper', 
+                                creator=lambda camper_obj: Signup(camper_id=camper_obj))
     
     # Add serialization rules
+    # serialization rules are to set recursion limits
+    serialize_rules = ('-signups.activity', '-signups.camper') 
+    serialize_only = ('id', 'name', 'difficulty')
+
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -39,11 +59,29 @@ class Camper(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
+
     # Add relationship
-    
+    signups = db.relationship("Signup", back_populates="camper", cascade='all, delete-orphan')
+    activities = db.relationship("Activity", secondary="signups", back_populates="campers")
+
+    activities = association_proxy('signups', 'activity',
+                                   creator=lambda activity_obj: Signup(activity_id=activity_obj))
     # Add serialization rules
-    
+    serialize_rules = ('-signups.camper', '-signups.activity')
+    serialize_only = ('id', 'name', 'age', 'signups')
+
     # Add validation
+    @validates('age')
+    def validates_age(self, key, age):
+        if not 8 <= age <= 18:
+            raise ValueError("Age must be in between 8 and 18")
+        return age
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name cannot be omitted")
+        return name
     
     
     def __repr__(self):
@@ -56,12 +94,24 @@ class Signup(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
 
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
+
     # Add relationships
-    
+    activity = db.relationship('Activity', back_populates='signups')
+    camper = db.relationship('Camper', back_populates='signups')
     # Add serialization rules
+    serialize_rules = ('-activity.signups','-camper.signups')
+
+    #serialize_only = ('id', 'time', 'activity_id', 'camper_id')
     
     # Add validation
-    
+    @validates('time')
+    def validates_time(self, key, time):
+        if not 0 <= time <= 23:
+            raise ValueError("Time must be in between 0 and 23.")
+        return time
+
     def __repr__(self):
         return f'<Signup {self.id}>'
 
